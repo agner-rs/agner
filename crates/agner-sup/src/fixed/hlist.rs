a725f135ceea363403abe8f585890d77c2e9142b
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-09-15 10:01:26 +0300

    basic HList

---
+++ crates/agner-sup/src/fixed/hlist.rs
@@ -0,0 +1,33 @@
+mod push;
+
+pub type Nothing = std::convert::Infallible;
+
+pub trait HList {
+    const LEN: usize;
+
+    type Head;
+    type Tail;
+}
+
+pub use push::PushFront;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Nil;
+
+#[derive(Debug, Clone)]
+pub struct Cons<H, T>(H, T);
+
+impl HList for Nil {
+    const LEN: usize = 0;
+    type Head = Nothing;
+    type Tail = Nothing;
+}
+
+impl<H, T> HList for Cons<H, T>
+where
+    T: HList,
+{
+    const LEN: usize = 1 + T::LEN;
+    type Head = H;
+    type Tail = T;
+}
