731f7de9b64afab1789d9f41dcde86999240f936
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-09-20 16:49:38 +0300

    ActorEntry refactored

--- crates/agner-actors/src/system/actor_entry.rs
+++ crates/agner-actors/src/system/actor_entry.rs
@@ -1,60 +1,107 @@
 use std::any::Any;
+use std::error::Error as StdError;
 
 use tokio::sync::mpsc;
 
 use crate::actor_id::ActorID;
 use crate::actor_runner::sys_msg::SysMsg;
-use crate::system::actor_id_pool::ActorIDLease;
-use crate::system::System;
+use crate::exit::Exit;
+
+use super::actor_id_pool::ActorIDLease;
 
 #[derive(Debug)]
-pub(crate) struct ActorEntry {
-    pub actor_id_lease: ActorIDLease,
-    pub sys_msg_tx: mpsc::UnboundedSender<SysMsg>,
-    pub messages_tx: Box<dyn Any + Send + Sync + 'static>,
+pub struct ActorEntry(Entry);
+
+#[derive(Debug)]
+enum Entry {
+    Vacant(Vacant),
+    Occupied(Occupied),
 }
 
-impl System {
-    pub(crate) async fn actor_entry_put(&self, entry: ActorEntry) {
-        if entry.actor_id_lease.system() != self.0.system_id {
-            panic!(
-                "attempt to insert an entry with a foreign actor-id [expected: {}; actual: {}]",
-                self.0.system_id,
-                entry.actor_id_lease.system()
-            )
-        }
+#[derive(Debug)]
+struct Occupied {
+    actor_id_lease: ActorIDLease,
+    messages_tx: Box<dyn Any + Send + Sync + 'static>,
+    sys_msg_tx: mpsc::UnboundedSender<SysMsg>,
+}
 
-        let slot_idx = entry.actor_id_lease.actor();
-        let mut slot = self.0.actor_entries[slot_idx].write().await;
-        let should_be_none = slot.replace(entry);
-        assert!(should_be_none.is_none());
-    }
-    pub(crate) async fn actor_entry_remove(&self, actor_id: ActorID) {
-        if actor_id.system() != self.0.system_id {
-            panic!(
-                "attempt to remove an entry with a foreign actor-id [expected: {}; actual: {}]",
-                self.0.system_id,
-                actor_id.system()
-            )
-        }
+type Vacant = Option<Terminated>;
 
-        let slot_idx = actor_id.actor();
-        let mut slot = self.0.actor_entries[slot_idx].write().await;
-        if slot.as_ref().map(|e| *e.actor_id_lease) == Some(actor_id) {
-            let should_be_some = slot.take();
-            assert!(should_be_some.is_some())
+#[derive(Debug)]
+struct Terminated {
+    actor_id: ActorID,
+    exit: Exit,
+}
+
+impl Default for ActorEntry {
+    fn default() -> Self {
+        Self(Entry::Vacant(Default::default()))
+    }
+}
+
+impl ActorEntry {
+    pub fn running_actor_id(&self) -> Option<ActorID> {
+        self.occupied().map(|oe| *oe.actor_id_lease)
+    }
+
+    pub fn running_or_terminated_actor_id(&self) -> Option<ActorID> {
+        match &self.0 {
+            Entry::Occupied(occupied) => Some(*occupied.actor_id_lease),
+            Entry::Vacant(Some(terminated)) => Some(terminated.actor_id),
+            Entry::Vacant(None) => None,
         }
     }
-    pub(crate) async fn actor_entry_read<F, Out>(&self, actor_id: ActorID, read: F) -> Option<Out>
-    where
-        F: FnOnce(&ActorEntry) -> Out,
+
+    pub fn messages_tx<M>(&self) -> Option<&mpsc::UnboundedSender<M>> 
+    where M: Send + Sync + 'static
     {
-        if actor_id.system() != self.0.system_id {
-            return None
+        self.occupied().and_then(|oe| oe.messages_tx.downcast_ref())
+    }
+    pub fn sys_msg_tx(&self) -> Option<&mpsc::UnboundedSender<SysMsg>> {
+        self.occupied().map(|oe| &oe.sys_msg_tx)
+    }
+}
+
+impl ActorEntry {
+    pub fn new<Message>(
+        actor_id_lease: ActorIDLease,
+        messages_tx: mpsc::UnboundedSender<Message>,
+        sys_msg_tx: mpsc::UnboundedSender<SysMsg>,
+    ) -> Self
+    where
+        Message: Send + Sync + 'static,
+    {
+        let occupied = Occupied { actor_id_lease, messages_tx: Box::new(messages_tx), sys_msg_tx };
+        let entry = Entry::Occupied(occupied);
+        Self(entry)
+    }
+
+    pub fn terminate(
+        &mut self,
+        actor_id: ActorID,
+        exit_reason: Exit,
+    ) -> Result<(), Box<dyn StdError + Send + Sync + 'static>> {
+        if self.running_actor_id() != Some(actor_id) {
+            Err("this entry does not have a running entry with the specified actor_id")?
         }
 
-        let slot_idx = actor_id.actor();
-        let slot = self.0.actor_entries.get(slot_idx)?.read().await;
-        slot.as_ref().filter(|e| *e.actor_id_lease == actor_id).map(read)
+        let to_terminate = std::mem::replace(&mut self.0, Entry::Vacant(Some(Terminated {
+            actor_id,
+            exit: exit_reason.to_owned(),
+        })));
+
+        
+
+        unimplemented!()
+    }
+}
+
+impl ActorEntry {
+    fn occupied(&self) -> Option<&Occupied> {
+        if let Entry::Occupied(occupied) = &self.0 {
+            Some(occupied)
+        } else {
+            None
+        }
     }
 }
---
+++ crates/agner-actors/src/system/sys_actor_entry.rs
@@ -0,0 +1,69 @@
+use std::ops::{Deref, DerefMut};
+
+use agner_utils::std_error_pp::StdErrorPP;
+
+use super::*;
+
+impl System {
+    pub(crate) async fn actor_entry_put(&self, entry: ActorEntry) {
+        let actor_id =
+            entry.running_actor_id().expect("Attempt to insert a non-running actor-entry");
+        assert_eq!(
+            actor_id.system(), self.0.system_id,
+            "attempt to insert an entry with a foreign actor-id [this-system-id: {}; entry-system-id: {}]",
+            self.0.system_id, actor_id.system());
+
+        let slot_idx = actor_id.actor();
+        let mut slot = self.0.actor_entries[slot_idx].write().await;
+        let should_be_none = std::mem::replace(&mut *slot, entry);
+        assert!(should_be_none.running_actor_id().is_none());
+    }
+
+    pub(crate) fn actor_entry_slot<'a>(&'a self, actor_id: ActorID) -> &'a RwLock<ActorEntry> {
+        if actor_id.system() != self.0.system_id {
+            panic!(
+                "attempt to terminate an entry with a foreign actor-id [this-system-id: {}; entry-system-id: {}]",
+                self.0.system_id,
+                actor_id.system()
+            )
+        }
+
+        let slot_idx = actor_id.actor();
+
+        &self.0.actor_entries[slot_idx]
+    }
+
+    pub(crate) async fn actor_entry_read<'a>(
+        &'a self,
+        actor_id: ActorID,
+    ) -> Option<impl Deref<Target = ActorEntry> + 'a> {
+        let locked = self.actor_entry_slot(actor_id).read().await;
+        if locked.running_or_terminated_actor_id() == Some(actor_id) {
+            Some(locked)
+        } else {
+            None
+        }
+    }
+    pub(crate) async fn actor_entry_write<'a>(
+        &'a self,
+        actor_id: ActorID,
+    ) -> Option<impl DerefMut<Target = ActorEntry> + 'a> {
+        let locked = self.actor_entry_slot(actor_id).write().await;
+        if locked.running_or_terminated_actor_id() == Some(actor_id) {
+            Some(locked)
+        } else {
+            None
+        }
+    }
+
+    pub(crate) async fn actor_entry_terminate(&self, actor_id: ActorID, exit_reason: Exit) {
+        if let Err(reason) = self
+            .actor_entry_write(actor_id)
+            .await
+            .map(|mut ae| ae.terminate(actor_id, exit_reason))
+            .transpose()
+        {
+            log::error!("Failed to terminate ActorEntry: {}", reason.as_ref().pp());
+        }
+    }
+}
