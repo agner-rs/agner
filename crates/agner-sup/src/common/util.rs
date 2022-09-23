3cfb3eac98cca6ea9438608f974f1d6fb83afe27
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-09-23 22:51:58 +0300

    https://github.com/RGafiyatullin/agner/security/code-scanning/39

--- crates/agner-sup/src/common/util.rs
+++ crates/agner-sup/src/common/util.rs
@@ -12,9 +12,7 @@ pub async fn try_exit(
     actor_id: ActorID,
     attempts: impl IntoIterator<Item = (Exit, Duration)>,
 ) -> Result<Exit, ExitFailed> {
-    let mut attempts = attempts.into_iter();
-
-    while let Some((exit, timeout)) = attempts.next() {
+    for (exit, timeout) in attempts {
         system.exit(actor_id, exit).await;
         if let Ok(actual_exit) = system.wait(actor_id).timeout(timeout).await {
             return Ok(actual_exit)
